61f2fe2d8639a3102c434b71d179b2dc75c5b221
cgansen <cgansen@gmail.com>
2013-02-08 20:03:57 -0600

    working, but hideous, tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
+var ward_singups = {
+  '1': 100,
+  '2': 200,
+  '3': 50
+}
+
+var ward_map;
+
 $(document).ready(function(){
-  var ward_map = $K.map('#ward-map');
-  ward_map.loadMap('assets/img/wards.svg', function() {
-       ward_map.addLayer('chicago');
-  });
+  ward_map = $K.map('#ward-map');
+  ward_map.loadMap('assets/img/wards.svg', function(){
+    ward_map.addLayer('chicago', {});
+    doMap();
+  });    
 });
+
+function doMap(){
+  $("path.chicago").each(function(idx, obj){
+    $(obj).tooltip({title: idx, 'container': 'body', 'placement': 'left'});
+  });  
+}
