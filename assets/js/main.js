0664b55eb2aae89c697f649c77fe134b86f4a980
Chris Gansen <cgansen@barackobama.com>
2013-02-10 20:47:55 -0600

    working tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,22 @@ var ward_map;
 $(document).ready(function(){
   ward_map = $K.map('#ward-map');
   ward_map.loadMap('assets/img/wards.svg', function(){
-    ward_map.addLayer('chicago', {});
+    ward_map.addLayer('chicago', {
+      key: "ward",
+      styles : {
+        'stroke-width': '0.75px',
+        fill: "#fff"
+      }
+    });
     doMap();
   });    
 });
 
 function doMap(){
+  var hover_color = "#ccc"
   $("path.chicago").each(function(idx, obj){
-    $(obj).tooltip({title: idx, 'container': 'body', 'placement': 'left'});
+    $(obj).bind("mouseenter", function(){ console.log("entered " + idx); $(obj).attr("fill", hover_color); });
+    $(obj).bind("mouseleave", function(){ console.log("left " + idx); $(obj).attr("fill", "#fff"); });
+    $(obj).tooltip({container: 'body', animation: false, placement: 'left', title: "Ward " + idx});
   });  
 }
