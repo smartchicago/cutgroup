d482cfff1248b4a7fdbc8113e1ae76ecd24e5592
Chris Gansen <cgansen@barackobama.com>
2013-02-10 21:40:45 -0600

    tooltips reading from data attrs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var ward_singups = {
+var ward_signups = {
   '1': 100,
   '2': 200,
   '3': 50
@@ -10,21 +10,36 @@ $(document).ready(function(){
   ward_map = $K.map('#ward-map');
   ward_map.loadMap('assets/img/wards.svg', function(){
     ward_map.addLayer('chicago', {
-      key: "ward",
       styles : {
         'stroke-width': '0.75px',
         fill: "#fff"
       }
     });
+    
     doMap();
   });    
 });
 
 function doMap(){
-  var hover_color = "#ccc"
-  $("path.chicago").each(function(idx, obj){
+  layer = ward_map.getLayer('chicago');
+
+  for(ward in ward_signups){
+    console.log("looking for " + ward)
+    var path = layer.getPaths({ward: ward});
+    console.log("got path: %o", path);
+
+    var node = path[0].svgPath.node;
+    jQuery.data(node,"signups", ward_signups[path[0].data.ward]);
+    jQuery.data(node,"ward", path[0].data.ward);    
+    console.log("%o --> ward %o, signups %o", node, path[0].data.ward, ward_signups[path[0].data.ward]);
+    console.log("data: %o", jQuery.data(node));
+  }
+
+
+  var hover_color = "#ccc"  
+  $("path.chicago").each(function(idx, obj){    
     $(obj).bind("mouseenter", function(){ console.log("entered " + idx); $(obj).attr("fill", hover_color); });
     $(obj).bind("mouseleave", function(){ console.log("left " + idx); $(obj).attr("fill", "#fff"); });
-    $(obj).tooltip({container: 'body', animation: false, placement: 'left', title: "Ward " + idx});
+    $(obj).tooltip({container: 'body', animation: false, placement: 'left', title: "Ward " + $.data(obj,'ward') + ": " +  ($.data(obj,'signups') || '0') + " testers" });
   });  
 }
