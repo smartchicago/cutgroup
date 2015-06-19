8018a61ab1173973ec41e51a85ef6e6f0ebac5fe
Josh Kalov <jkalov@gmail.com>
2015-06-18 22:32:45 -0500

    Switch to Chicago+Cook Municipality map

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ S3CMD = s3cmd -c .s3cfg \
 	--no-delete-removed \
 	--reduced-redundancy \
 	--progress \
-	--rexclude '\.git|s3cfg|Makefile|signups.json|.DS_Store' \
+	--rexclude '\.git|s3cfg|Makefile|.DS_Store' \
 	sync ./
 
 all: staging
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     $.get('assets/js/signups.json', function(data) { 
       ward_signups = data;
       ward_map = $K.map('#ward-map');
-      ward_map.loadMap('assets/img/wards.svg', function(){
+      ward_map.loadMap('assets/img/combined.svg', function(){
         ward_map.addLayer('chicago', {
           styles : {
             'stroke-width': '0.75px',
@@ -41,14 +41,15 @@ function doMap(){
     var path = layer.getPaths({ward: ward});
     var node = path[0].svgPath.node;
     jQuery.data(node,"signups", ward_signups[path[0].data.ward]);
-    jQuery.data(node,"ward", path[0].data.ward);    
+    jQuery.data(node,"ward", path[0].data.ward);
+    jQuery.data(node,"displaynam", path[0].data.displaynam);    
   }
 
   // colors!
   ward_map.getLayer('chicago').style({
     fill: function(d){ 
       if(ward_signups[d.ward] == 0){
-          return "#fff";
+          return "#ccc";
         } else {
           return color_for_value(ward_signups[d.ward]);
         }
@@ -61,6 +62,6 @@ function doMap(){
     $(obj).bind("mouseleave", function(){ $(obj).attr("fill", $(obj).data('previous-fill-color')); });
     
     var number_of_signups = $.data(obj,'signups');
-    $(obj).tooltip({container: 'body', animation: false, placement: 'left', title: "Ward " + $.data(obj,'ward') + ": " +  (number_of_signups || '0') + " tester" + (number_of_signups == 1 ? '' : 's') });
+    $(obj).tooltip({container: 'body', animation: false, placement: 'left', title: $.data(obj,'displaynam') + ": " +  (number_of_signups || '0') + " tester" + (number_of_signups == 1 ? '' : 's') });
   });  
 }
